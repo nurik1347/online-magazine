50338410e9cfcd7770835c9bc07f2eda2a01bef1
nurik1347 <nurikmirzaev1747@gmail.com>
2026-03-23 10:42:17 +0500

    fix: spa refresh 404 on vercel

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import { useAuthStore } from "../stores/auth";
 
 import LoginView from "../views/LoginView.vue";
@@ -108,7 +108,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(),
     routes
 })
 
